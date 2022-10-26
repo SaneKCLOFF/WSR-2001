2db1697e647c60075d53ed6c81c704403e4b2c51
Gsgshn <100374838+Gsgshn@users.noreply.github.com>
2022-10-26 11:01:08 +0300

    v0.1

---
+++ Project/Axyonov.Lopushok/Axyonov.Lopushok/Domain/Entities/Prob.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Axyonov.Lopushok.Domain.Entities
+{
+    public partial class Prob
+    {
+        public string Продукция { get; set; } = null!;
+        public string НаимованиеМатериала { get; set; } = null!;
+        public string НеобходимоеКоличествоМатериала { get; set; } = null!;
+    }
+}
