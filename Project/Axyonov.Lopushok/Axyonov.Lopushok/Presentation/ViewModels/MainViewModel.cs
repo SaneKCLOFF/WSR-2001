0e95bce218e6b3e1cd8e55bf9e1a8c636c1586f5
SaneKCLOFF <sanekcl.sanek@gmail.com>
2022-10-27 12:50:47 +0300

    v0.3

--- Project/Axyonov.Lopushok/Axyonov.Lopushok/Presentation/ViewModels/MainViewModel.cs
+++ Project/Axyonov.Lopushok/Axyonov.Lopushok/Presentation/ViewModels/MainViewModel.cs
@@ -1,89 +1,102 @@
-﻿using Axyonov.Lopushok.Infrastructure.Persistence;
+﻿using Axyonov.Lopushok.Domain.Entities;
+using Axyonov.Lopushok.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Axyonov.Lopushok.Presentation.ViewModels;
-using Axyonov.Lopushok.Domain.Entities;
+using System.Windows;
 
 namespace Axyonov.Lopushok.Presentation.ViewModels
 {
-    internal class MainViewModel:ViewModelBase
+    internal class MainViewModel : ViewModelBase
     {
+        #region Fields
         public List<Product> _products = new();
         private List<string> _sorterItemsList = new();
         private List<string> _filtherItemsList = new();
-        private string _selectedSorter=null!;
-        private string _selectedFilter=null!;
-        public List<Product> Products 
+        private string _selectedSorter = null!;
+        private string _selectedFilter = null!;
+        private string _search = null!;
+        public List<Product> Products
         {
             get => _products;
-            set 
+            set
             {
                 _products = value;
                 OnPropertyChanged(nameof(Products));
-            } 
+            }
         }
-        public List<string> SorterItemsList 
-        { 
+        public List<string> SorterItemsList
+        {
             get => _sorterItemsList;
-            set => _sorterItemsList = value; 
+            set => _sorterItemsList = value;
         }
-        public List<string> FiltherItemsList 
-        { 
-            get => _filtherItemsList; 
-            set => _filtherItemsList = value; 
+        public List<string> FiltherItemsList
+        {
+            get => _filtherItemsList;
+            set => _filtherItemsList = value;
         }
-        public string SelectedSorter 
-        { 
+        public string SelectedSorter
+        {
             get => _selectedSorter;
             set
             {
                 _selectedSorter = value;
-                SortProducts(value);
+                Products = SortProducts(GetProducts(), value);
                 OnPropertyChanged(nameof(SelectedSorter));
-            } 
+            }
         }
-        public string SelectedFilter 
-        { 
+        public string SelectedFilter
+        {
             get => _selectedFilter;
-            set 
+            set
             {
                 _selectedFilter = value;
-                FilthProducts(value);
+                Products = FilthProducts(GetProducts(), value);
                 OnPropertyChanged(nameof(SelectedFilter));
             }
         }
 
+        public string Search
+        {
+            get => _search;
+            set
+            {
+                _search = value;
+                Products = SearchProducts(GetProducts(), value);
+                OnPropertyChanged(nameof(Search));
+            }
+        }
+        #endregion
         public MainViewModel()
         {
             #region Загрузка данных в список
             Products = GetProducts();
             SorterItemsList = GetSorterItems();
             FiltherItemsList = GetFiltherItems();
-            SelectedSorter ="Без сортировки";
+            SelectedSorter = "Без сортировки";
             SelectedFilter = "Без фильтров";
             #endregion
         }
+        #region Сортировка, фильтрация, поиск
         private List<Product> GetProducts()
         {
             using (ApplicationDbContext context = new())
             {
-               return context.Products
-                    .Include(pt => pt.ProductType)
-                    .Include(pm => pm.ProductMaterials)
-                    .ThenInclude(m => m.Material)
-                    .ToList();
-            } 
+                return context.Products
+                     .Include(pt => pt.ProductType)
+                     .Include(pm => pm.ProductMaterials)
+                     .ThenInclude(m => m.Material)
+                     .ToList();
+            }
         }
         private List<string> GetSorterItems()
         {
             List<string> Sorters = new();
             Sorters.Add("Без сортировки");
-            Sorters.Add("По возрастанию");
-            Sorters.Add("По убыванию");
+            Sorters.Add("По возрастанию стоимости");
+            Sorters.Add("По убыванию стоимости");
+            Sorters.Add("По названию (возр.)");
+            Sorters.Add("По названию (убыв.)");
             return Sorters;
         }
         private List<string> GetFiltherItems()
@@ -98,34 +111,109 @@ namespace Axyonov.Lopushok.Presentation.ViewModels
                 }
             }
             return Filthers;
-        }   
-        private void SortProducts(string value)
+        }
+        private List<Product> SortProducts(List<Product> lProducts, string value)
         {
-            if (value=="Без сортировки")
+            List<Product> SortList = new();
+            if (value == "Без сортировки")
             {
-                Products = GetProducts();
+                SortList = GetProducts();
             }
-            else if (value=="По возрастанию")
+            else if (value == "По возрастанию стоимости" && (Search != null || Search != string.Empty))
             {
-                Products = GetProducts().OrderBy(p => p.Title).ToList();
+                SortList = lProducts.OrderBy(p => p.TotalCost).ToList();
+                SortList=SearchProducts(SortList, Search);
             }
-            else if (value=="По убыванию")
+            else if (value == "По возрастанию стоимости")
             {
-                Products = GetProducts().OrderByDescending(p => p.Title).ToList();
+                SortList = lProducts.OrderBy(p => p.TotalCost).ToList();
             }
+            else if (value == "По убыванию стоимости" && (Search != null || Search != string.Empty))
+            {
+                SortList = lProducts.OrderByDescending(p => p.TotalCost).ToList();
+                SortList = SearchProducts(SortList, Search);
+            }
+            else if (value == "По убыванию стоимости")
+            {
+                SortList = lProducts.OrderByDescending(p => p.TotalCost).ToList();
+            }
+            else if (value == "По названию (возр.)" && (Search != null || Search != string.Empty))
+            {
+                SortList = lProducts.OrderBy(p => p.Title).ToList();
+                SortList = SearchProducts(SortList, Search);
+            }
+            else if (value == "По названию (возр.)")
+            {
+                SortList = lProducts.OrderBy(p => p.Title).ToList();
+            }
+            else if (value == "По названию (убыв.)" && (Search != null || Search != string.Empty))
+            {
+                SortList = lProducts.OrderByDescending(p => p.Title).ToList();
+            }
+            else if (value == "По названию (убыв.)")
+            {
+                SortList = lProducts.OrderByDescending(p => p.Title).ToList();
+                SortList = SearchProducts(SortList, Search);
+            }
+            return SortList;
         }
 
-        private void FilthProducts(string value)
+        private List<Product> FilthProducts(List<Product> lProducts, string value)
         {
-            if (value=="Без фильтров")
+            List<Product> FilthList = new();
+            if (value == "Без фильтров")
             {
-                Products = GetProducts();
+                FilthList = lProducts;
+            }
+            else if (value == "Без фильтров" && (Search != null || Search != string.Empty))
+            {
+                FilthList = SearchProducts(FilthList, Search);
+            }
+            else if (Search != null || Search != string.Empty)
+            {
+                FilthList = lProducts.Where(t => t.ProductType.Title == value).ToList();
+                FilthList = SearchProducts(FilthList, Search);
             }
             else
             {
-                Products = GetProducts().Where(t => t.ProductType.Title == value).ToList();
+                FilthList = lProducts.Where(t => t.ProductType.Title == value).ToList();
+            }
+            return FilthList;
+        }
+        private List<Product> SearchProducts(List<Product> lProducts, string value)
+        {
+            List<Product> SearchList = new();
+            if (value == string.Empty || value == null)
+            {
+                SearchList = lProducts;
+            }
+            else
+            {
+                SearchList = lProducts.Where(ptitle => ptitle.Title.ToLower().Contains(value.ToLower())).ToList();
+            }
+            return SearchList;
+        }
+        #endregion
+
+        #region Commands
+        private RelayCommand _openAddShelfWindowCommand;
+        public RelayCommand OpenAddShelfWindowCommand
+        {
+            get
+            {
+                return _openAddShelfWindowCommand ?? new RelayCommand(obj =>
+                {
+                    OpenAddShelfWindow();
+                });
             }
         }
+        private void OpenAddShelfWindow()
+        {
+            AddShelfWindow addShelfWindow = new AddShelfWindow();
+            addShelfWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            addShelfWindow.ShowDialog();
+        }
+        #endregion
     }
 
 }
